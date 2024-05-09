b9cb1a77c05a3ae9e8ed76a0255a1f17bcb73414
Bill Stark <billstark001@gmail.com>
2024-05-10 05:56:27 +0900

    env

--- +utils/env.m
+++ +utils/env.m
@@ -1,4 +1,63 @@
-function env_vars = env(path)
+function env_vars = env(varargin)
+    p = inputParser();
+    addParameter(p, 'SysMethod', 'system');
+    addParameter(p, 'File', '');
+    parse(p, varargin{:})
+    r = p.Results;
+    env_vars = env_sys(r.SysMethod);
+    if ~isempty(r.File)
+        env_vars_f = env_file(r.File);
+        for k = fieldnames(env_vars_f)
+            k_ = k{:};
+            env_vars.(k_) = env_vars_f.(k_);
+        end
+    end
+end
+
+function env_vars = env_sys(method)
+    if nargin < 1, method = 'system'; end
+    method = validatestring(method, {'java', 'system'});
+
+    switch method
+        case 'java'
+            map = java.lang.System.getenv();  % returns a Java map
+            keys = cell(map.keySet.toArray());
+            vals = cell(map.values.toArray());
+        case 'system'
+            if ispc()
+                %cmd = 'set "';  %HACK for hidden variables
+                cmd = 'set';
+            else
+                cmd = 'env';
+            end
+            [~,out] = system(cmd);
+            vars = regexp(strtrim(out), '^(.*)=(.*)$', ...
+                'tokens', 'lineanchors', 'dotexceptnewline');
+            vars = vertcat(vars{:});
+            keys = vars(:,1);
+            vals = vars(:,2);
+    end
+
+    % Windows environment variables are case-insensitive
+    if ispc()
+        keys = upper(keys);
+    end
+
+    env_vars = struct();
+    for i = 1:numel(keys)
+        k = keys(i);
+        k = k{:};
+        v = vals(i);
+        v = v{:};
+        if ~isvarname(k)
+            continue
+        end
+        env_vars.(k) = v;
+    end
+end
+
+
+function env_vars = env_file(path)
 
     fp = fopen(path);
     env_file = textscan(fp, '%s', 'delimiter', '\n');
